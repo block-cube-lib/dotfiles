78a1c2b4c44a0c98714c0816e0d1f60bdef7453b
block <block.cube.lib@gmail.com>
2019-10-28 14:41:10 +0900

    feat Add fish.config

---
+++ fish/config.fish
@@ -0,0 +1,35 @@
+function add_path_if_need
+  set result (false)
+  for v in $argv
+    contains $v $PATH -- or (contains $v $fish_user_paths)
+    if test $status -ne 0
+      echo "Add $v to $fish_user_paths"
+      set -x fish_user_paths $v fish_user_paths
+      set result (true)
+    end
+  end
+
+  return $result
+end
+
+function setup_rust
+  type -q cargo
+  if test $status -ne 0
+    echo "install rust"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  end
+  add_path_if_need $HOME/.cargo/bin
+end
+
+function setup_starship
+  type -q starship
+  if test $status -ne 0
+    echo "install starship"
+    cargo install starship
+  end
+
+  eval (starship init fish)
+end
+
+setup_rust
+setup_starship
