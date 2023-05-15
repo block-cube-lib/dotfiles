3f0530b23961aad7c55f75b5bf10039624e4988b
block <block.cube.lib@gmail.com>
2023-05-15 23:30:04 +0900

    feat: merge neovim setting files

--- nvim/lua/key_mapping.lua
+++
@@ -1,9 +0,0 @@
--- Plugin-independent key mapping settings.
-
-vim.g.mapleader = ' '
-
--- open terminal
-vim.keymap.set('n', '<Leader>tt', [[<Cmd>tabnew<CR><Cmd>terminal<CR>]], { noremap = true, silent = true })
-
--- terminal
-vim.keymap.set('t', '<C-Q>', [[<C-\><C-n>]], { noremap = true, silent = true })
