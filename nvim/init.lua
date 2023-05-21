a59bf53dad01daf13a3e04bc6b83e73a5842fb3e
block <block.cube.lib@gmail.com>
2023-05-21 15:09:55 +0900

    feat: Modify lualine filepath view setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -176,7 +176,7 @@ local plugins = {
 			sections = {
 				lualine_a = {'mode'},
 				lualine_b = {'branch', 'diff', 'diagnostics'},
-				lualine_c = {'filename', 'lsp_progress'},
+				lualine_c = {{'filename', path = 1}, 'lsp_progress'},
 				lualine_x = {'encoding', 'fileformat', 'filetype'},
 				lualine_y = {'progress'},
 				lualine_z = {'location'}
