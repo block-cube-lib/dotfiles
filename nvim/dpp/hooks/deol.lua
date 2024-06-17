a8440423dd57b286a50c491dca930c5c5f6534ce
masato.tanaka <masato.tanaka@ponos.co.jp>
2024-06-17 14:23:03 +0900

    feat: Update deol settings

--- nvim/dpp/hooks/deol.lua
+++ nvim/dpp/hooks/deol.lua
@@ -18,13 +18,30 @@ vim.keymap.set('n', '<Leader>tv', deol_command({ split = 'vertical' }), opt)
 vim.keymap.set('n', '<Leader>tr', deol_command({ split = 'farright' }), opt)
 vim.keymap.set('n', '<Leader>tl', deol_command({ split = 'farleft' }), opt)
 
-vim.g["deol#prompt_pattern"] = "❯ ";
+vim.g["deol#prompt_pattern"] = ">";
 vim.g["deol#extra_options"] = { term_finish = 'close' }
 
 vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'deol' },
+	callback = function()
+		local opt = { noremap = true, buffer = true, silent = true };
+		vim.keymap.set('n', '<C-n>', [[<Plug>(deol_next_prompt]], opt)
+		vim.keymap.set('n', '<C-p>', [[<Plug>(deol_previous_prompt)]], opt)
+		vim.keymap.set('n', '<CR>', [[<Plug>(deol_execute_line)]], opt)
+		vim.keymap.set('n', 'A', [[<Plug>(deol_start_append_last)]], opt)
+		vim.keymap.set('n', 'I', [[<Plug>(deol_start_insert_first)]], opt)
+		vim.keymap.set('n', 'a', [[<Plug>(deol_start_append)]], opt)
+		vim.keymap.set('n', 'e', [[<Plug>(deol_edit)]], opt)
+		vim.keymap.set('n', 'i', [[<Plug>(deol_start_insert)]], opt)
+		vim.keymap.set('n', 'q', [[<Plug>(deol_quit)]], opt)
+	end
+})
+vim.api.nvim_create_autocmd('FileType', {
 	pattern = { 'deol-edit' },
 	callback = function(ev)
-		vim.keymap.set('i', '<C-Q>', [[<Esc>]], { noremap = true, silent = true })
+		local opt = { noremap = true, buffer = true, silent = true }
+		vim.keymap.set('i', '<C-Q>', [[<Esc>]], opt)
+		vim.keymap.set({'n', 'i'}, '<CR>', [[<Plug>(deol_execute_line)]], opt)
 	end,
 })
 -- }}}
