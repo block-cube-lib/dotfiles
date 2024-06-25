6a1773b433e5637cb0b13e3cc0c0fb86e7708ec5
masato.tanaka <masato.tanaka@ponos.co.jp>
2024-06-25 13:05:19 +0900

    feat: Remove c-j mapping in insert mode on VSCode

--- vscode-neovim/init.lua
+++ vscode-neovim/init.lua
@@ -3,6 +3,8 @@ vim.opt.runtimepath:append(nvim_base_dir)
 
 require("base")
 
+vim.keymap.set('i', '<C-j>', '')
+
 if vim.g.vscode then
 	vim.keymap.set('n', 'gd', [[<Cmd>call VSCodeCall('editor.action.revealDefinition')<CR>]],
 		{ noremap = true, silent = true })
