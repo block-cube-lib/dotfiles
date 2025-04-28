9a995f033c5aa17a501897a90acfc7ba7099101d
block <block.cube.lib@gmail.com>
2025-04-28 11:55:08 +0900

    feat: change keymap for neo-tree

--- nvim/dpp/hooks/neo-tree.lua
+++ nvim/dpp/hooks/neo-tree.lua
@@ -1,9 +1,8 @@
 -- lua_add {{{
-vim.keymap.set('n', '<Leader>F', function()
-	if vim.bo.filetype == 'neo-tree' then
-		vim.cmd('Neotree close')
-	else
-		vim.cmd('Neotree')
-	end
+vim.keymap.set('n', '<Leader>Fo', function()
+	vim.cmd('Neotree')
+end, { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>Fc', function()
+	vim.cmd('Neotree close')
 end, { noremap = true, silent = true })
 -- }}}
