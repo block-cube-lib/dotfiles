9156d4b2a2ba5fa9b919fa27f78d4bfa6af1f5aa
block <block.cube.lib@gmail.com>
2025-08-18 15:02:18 +0900

    fix: Fix format method

--- nvim/dpp/hooks/nvim-lspconfig.lua
+++ nvim/dpp/hooks/nvim-lspconfig.lua
@@ -95,7 +95,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, opts)
 		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 		vim.keymap.set('n', '<Leader>f', function()
-			vim.lsp.buf.format { async = true }
+			vim.lsp.buf.format({ timeout_ms = 2000 })
 		end, opts)
 	end,
 })
